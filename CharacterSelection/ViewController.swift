2e1d1d75430300284b868dc91cc2ead6c04ea358
Jay <prosnipa1221@gmail.com>
2021-08-30 07:14:32 -0500

    newer images

--- CharacterSelection/ViewController.swift
+++ CharacterSelection/ViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 class ViewController: UIViewController {
-var tester = 5
     @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func pikaButton(_ sender: Any) {
+        image.image = UIImage(named: "pikachu")
+    }
     
-
+    @IBAction func gokuButton(_ sender: Any) {
+    }
+    @IBAction func saitButton(_ sender: Any) {
+    }
+    
 
 }
 
